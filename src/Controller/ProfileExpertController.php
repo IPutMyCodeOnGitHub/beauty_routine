692e7f8875f47669403e5363ca30cebe48f96c48
victoria_strepeltsova <victoria.str@inbox.ru>
2019-12-06 14:04:54 +0700

    merged

--- src/Controller/ProfileExpertController.php
+++ src/Controller/ProfileExpertController.php
@@ -2,21 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/profile-expert", name="profile.")
+ * @Route("/profile-expert")
  */
 class ProfileExpertController extends AbstractController
 {
     /**
-     * @Route("/", name="expert")
+     * @Route("", name="profile.expert")
      */
     public function index()
     {
-        return $this->render('profile-expert/index.html.twig', [
+        /** @var User $user */
+        $user = $this->getUser();
+        $userId = $user->getId();
+
+        // symfony/finder
+        $filesystem = new Filesystem();
+        $path = "/certificate/$userId";
+        $certificateDir = getcwd() . $path;
+        if ($filesystem->exists($certificateDir)) {
+            $finder = new Finder();
+            $finder->files()->in($certificateDir);
+
+            $filesPaths = [];
+            if ($finder->hasResults()) {
+                foreach ($finder as $file) {
+                    $filesPaths[] = $path .'/' . $file->getRelativePathname();
+                }
+            }
+        }
+        return $this->render('profile-expert/profile-expert.html.twig', [
             'controller_name' => 'ProfileExpertController',
+            'name' => $user->getName(),
+            'email' => $user->getEmail(),
+            'id' => 'userId = ' . $userId,
+            'certificatesPaths' => $filesPaths,
+        ]);
+    }
+
+    /**
+     * @Route("/edit", name="profile.expert.edit")
+     */
+    public function profileExpertEdit()
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        return $this->render('profile-expert/profile-expert-edit.html.twig', [
+            'controller_name' => 'ProfileExpertController',
+            'name' => $user->getName(),
+            'email' => $user->getEmail(),
         ]);
     }
 }
